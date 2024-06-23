a754d400491f19397b009664aec36890e43e9a71
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-06-23 00:01:14 -0300

    feat: implements category and product controllers.

--- VShop.ProductApi/Controllers/CategoryController.cs
+++ VShop.ProductApi/Controllers/CategoryController.cs
@@ -1,10 +1,86 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Services.Interfaces;
 
 namespace VShop.ProductApi.Controllers;
 
 [Route("[controller]")]
 [ApiController]
 [Produces("application/json")]
-public class CategoryController : ControllerBase
+public class CategoryController(ICategoryService categoryService) : ControllerBase
 {
+
+    // GET /category
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var categories = await categoryService.Get();
+
+        return Ok(categories);
+    }
+
+    // GET /category/{id}
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var category = await categoryService.GetById(id);
+
+        if (category == null)
+        {
+            return NotFound("Category not found.");
+        }
+
+        return Ok(category);
+    }
+
+    // POST /category
+    [HttpPost]
+    public async Task<IActionResult> Add(CategoryDTO dto)
+    {
+        if (dto == null)
+        {
+            return BadRequest("Invalid data.");
+        }
+
+        var category = await categoryService.Add(dto);
+
+        return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+    }
+
+    // PUT /category
+    [HttpPut]
+    public async Task<IActionResult> Update(int id, CategoryDTO dto)
+    {
+        if (dto == null || id != dto.Id)
+        {
+            return BadRequest("Invalid data.");
+        }
+
+        var category = await categoryService.GetById(id);
+
+        if (category == null)
+        {
+            return NotFound("Category not found.");
+        }
+
+        await categoryService.Update(dto);
+
+        return Ok("Category updated successfully.");
+    }
+
+    // DELETE /category
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await categoryService.GetById(id);
+
+        if (category == null)
+        {
+            return NotFound("Category not found.");
+        }
+
+        await categoryService.Delete(category);
+
+        return Ok("Category deleted successfully.");
+    }
 }
--- VShop.ProductApi/Controllers/ProductController.cs
+++ VShop.ProductApi/Controllers/ProductController.cs
@@ -1,5 +1,85 @@
-﻿namespace VShop.ProductApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Services.Interfaces;
 
-public class ProductController
+namespace VShop.ProductApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+[Produces("application/json")]
+public class ProductController(IProductService productService) : ControllerBase
 {
+    // GET /product
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var products = await productService.Get();
+
+        return Ok(products);
+    }
+
+    // GET /product/{id}
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var product = await productService.GetById(id);
+
+        if (product == null)
+        {
+            return NotFound("Product not found.");
+        }
+
+        return Ok(product);
+    }
+
+    // POST /product
+    [HttpPost]
+    public async Task<IActionResult> Add(ProductDTO dto)
+    {
+        if (dto == null)
+        {
+            return BadRequest("Invalid data.");
+        }
+
+        var product = await productService.Add(dto);
+
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+
+    // PUT /product
+    [HttpPut]
+    public async Task<IActionResult> Update(Guid id, ProductDTO dto)
+    {
+        if (dto == null || id != dto.Id)
+        {
+            return BadRequest("Invalid data.");
+        }
+
+        var product = await productService.GetById(id);
+
+        if (product == null)
+        {
+            return NotFound("Product not found.");
+        }
+
+        await productService.Update(dto);
+
+        return Ok("Product updated successfully.");
+    }
+
+    // DELETE /product
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var product = await productService.GetById(id);
+
+        if (product == null)
+        {
+            return NotFound("Product not found.");
+        }
+
+        await productService.Delete(product);
+
+        return Ok("Product deleted successfully.");
+    }
 }
