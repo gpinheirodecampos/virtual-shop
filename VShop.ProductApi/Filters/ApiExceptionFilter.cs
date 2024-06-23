002316868039bd4f3f81a5ef6ac34c8b6c37b177
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-06-23 00:01:48 -0300

    feat: implements logs.

---
+++ VShop.ProductApi/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VShop.ProductApi.Filters;
+
+public class ApiExceptionFilter(ILogger<ApiExceptionFilter> logger) : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        logger.LogError(context.Exception, "Ocorreu uma exceção não tratada: Status Code 500");
+
+        context.Result = new ObjectResult("Ocorreu um problema ao tratar a sua solicitação: Status Code 500")
+        {
+            StatusCode = StatusCodes.Status500InternalServerError,
+        };
+    }
+}
---
+++ VShop.ProductApi/Filters/ApiLoggingFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace VShop.ProductApi.Filters;
+
+public class ApiLoggingFilter : IActionFilter
+{
+    // Injecao de dependencia
+    private readonly ILogger _logger;
+    public ApiLoggingFilter(ILoggerFactory logger)
+    {
+        _logger = logger.CreateLogger(""); ;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        // montando log
+        var logRequest = context.HttpContext.Request.Method + " " + context.HttpContext.Request.Scheme + "://" + context.HttpContext.Request.Host + context.HttpContext.Request.Path;
+
+        var logResponse = context.HttpContext.Response.StatusCode == 200 ? "200 OK!" : $"{context.HttpContext.Response.StatusCode} FAIL!";
+
+        _logger.LogInformation("##################################");
+        _logger.LogInformation("### Executando -> OnActionExecuting");
+        _logger.LogInformation($"### Horário: {DateTime.Now.ToLongTimeString()}");
+        _logger.LogInformation($"### Requisição: {logRequest}");
+        _logger.LogInformation($"### Resposta: {logResponse}");
+        _logger.LogInformation("##################################");
+    }
+
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        var logRequest = context.HttpContext.Request.Method + " " + context.HttpContext.Request.Scheme + "://" + context.HttpContext.Request.Host + context.HttpContext.Request.Path;
+
+        var logResponse = context.HttpContext.Response.StatusCode == 200 ? "200 OK!" : $"{context.HttpContext.Response.StatusCode} FAIL!";
+
+        _logger.LogInformation("##################################");
+        _logger.LogInformation("### Executando -> OnActionExecuted");
+        _logger.LogInformation($"### Horário: {DateTime.Now.ToLongTimeString()}");
+        _logger.LogInformation($"### Requisição: {logRequest}");
+        _logger.LogInformation($"### Resposta: {logResponse}");
+        _logger.LogInformation("##################################");
+    }
+}
